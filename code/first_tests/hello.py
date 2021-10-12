d196e2148e6470c6094e85cb3556599a50bf39bd
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-10-12 09:49:37 +0300

    change ./code dir

--- code/first_tests/hello.py
+++ code/first_tests/hello.py
@@ -1,1 +1,7 @@
 print("Hello world!")
+example_string = input("Input some string:")
+print(f"String in upper case: {example_string.upper()}")
+print("Evaluating square perimeter:")
+square_side = int(input("Please input square side size:"))
+square_perimeter = square_side * 4
+print(f"Square perimeter is: {square_perimeter}")
---
+++ code/first_tests/string_methods.py
@@ -0,0 +1,9 @@
+# This file explains string methods in Python3.
+print("Создание строки: some_string = 'some new string'")
+some_string = 'some new string'
+print(f'some_string : {some_string}')
+print("Конкатенация строк: some_strint + another_string")
+another_string = 'some another string'
+print(f'some_string : {some_string}, another_string : {another_string}')
+new_line = '\n'
+print(f"Мультипликация строк: \n{some_string} {another_string} {new_line}", sep=" ")
