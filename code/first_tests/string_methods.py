b8891f5ac6bf6a79e9ccab5904161c22a4428643
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-10-13 19:08:17 +0300

    change string_mehonds.py

--- code/first_tests/string_methods.py
+++ code/first_tests/string_methods.py
@@ -1,9 +1,9 @@
-# This file explains string methods in Python3.
-print("Создание строки: some_string = 'some new string'")
-some_string = 'some new string'
-print(f'some_string : {some_string}')
+""" This file explains string methods in Python3."""
+
+SOME_STRING = 'some new string'
+print(f'some_string : {SOME_STRING}')
 print("Конкатенация строк: some_strint + another_string")
-another_string = 'some another string'
-print(f'some_string : {some_string}, another_string : {another_string}')
-new_line = '\n'
-print(f"Мультипликация строк: \n{some_string} {another_string} {new_line}", sep=" ")
+ANOTHER_STRING = 'some another string'
+print(f'some_string : {SOME_STRING}, another_string : {ANOTHER_STRING }')
+NEW_LINE = '\n'
+print(f"Мультипликация строк: \n{SOME_STRING} {ANOTHER_STRING} {NEW_LINE} ", sep=" ")
