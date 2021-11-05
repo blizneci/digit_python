28dccd3dae6c46e5df07d3fb35f88487c24bc1ae
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-11-05 18:16:44 +0300

    change exercise_1.py

--- chapter_9_python/9_11_dictionaries/exercise_1.py
+++ chapter_9_python/9_11_dictionaries/exercise_1.py
@@ -8,8 +8,13 @@
 import json
 
 
-my_dict = json.loads(input("Введите словарь\n"))
+#my_dict = json.loads(input("Введите словарь ниже:\n"))
+my_dict = json.loads('{"key1": 40, "key2": 45, "key3": 100, "key4": 555}')
+print(f"Введенный словарь: {my_dict}")
 MIN_VALUE = min(my_dict.values())
-for key, value in my_dict.items():
-    if value == MIN_VALUE:
-        print(key)
+print(f"Минимальное значение словаря: {MIN_VALUE}")
+#   for key, value in my_dict.items():
+#       if value == MIN_VALUE:
+#           print(key)
+KEY_MIN_VALUE = min(my_dict.items(), key=lambda i : i[1])[0]
+print(f"Ключ словаря с минимальным значением: {KEY_MIN_VALUE}")
