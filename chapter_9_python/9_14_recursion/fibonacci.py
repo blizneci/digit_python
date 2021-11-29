0aeee3c4f3d9a5950f36aa9301feb508e59789a0
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-11-29 23:53:44 +0300

    change fibonacci.py

--- chapter_9_python/9_14_recursion/fibonacci.py
+++ chapter_9_python/9_14_recursion/fibonacci.py
@@ -1,5 +1,25 @@
 """Модуль для поиска чисел Фибоначчи
 Рекурсивная и нерекурсивная реализация алгоритма поиска."""
 
-n = int(input('Введите номер числа Фибоначчи'))
+def get_fib_rec(n):
+    """Рекурсивный поиск n-го числа Фибоначчи"""
+    if n in (1, 2):
+        return 1
+    else:
+        return get_fib_rec(n - 1) + get_fib_rec(n - 2)
+
+def get_fib_iter(n):
+    fib1 = fib2 = 1
+    if n in (1, 2):
+        return 1
+    else:
+        for i in range(0, n - 2):
+            fib = fib1 + fib2
+            fib1, fib2 = fib2, fib1 + fib2
+        return fib
+
+
+n = int(input('Введите номер числа Фибоначчи \n'))
 print(f'Поиск {n}-го числа Фибоначчи')
+print(f'Рекурсивный поиск: {get_fib_rec(n)}')
+print(f'Итеративный поиск: {get_fib_iter(n)}')
