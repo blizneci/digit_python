45b132334a10ca79355a2c6f5e7bad70059c8327
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-12-06 17:47:10 +0300

    change exercise_2.py, add docstrings

--- chapter_9_python/9_16_objects/exercise_2.py
+++ chapter_9_python/9_16_objects/exercise_2.py
@@ -2,22 +2,23 @@ import random
 import string
 
 class User():
-    """docstring"""
+    """Создает пользователя."""
     def __init__(self, name: str, age: int):
-        """docstring"""
+        """Иницализация пользователя с именем name и возрастом age."""
         self.name = name
         self.age = age
 
     def is_adult(self) -> bool:
-        """docstring"""
+        """Проверяет, является ли пользователь совершеннолетним"""
         return self.age >= 18
 
     @staticmethod
     def generate_password(length: int) -> str:
-        """docstring"""
+        """Генерирует пароль с переданной длиной length."""
         return ''.join(random.sample(string.ascii_letters + string.digits, length))
 
     def get_name(self) -> str:
+        """Возвращает имя пользователя."""
         return self.name
 
 user = User('John', 19)
