06dfd5215eb74be131ddd5ea02b3d088f523ffba
Kirill Zhelvakov <blzneci.89@gmail.com>
2021-12-15 17:52:34 +0300

    change dates.py

--- chapter_9_python/9_20_dates/dates.py
+++ chapter_9_python/9_20_dates/dates.py
@@ -2,26 +2,32 @@ import datetime
 import pytz
 from dateutil.relativedelta import relativedelta
 
-#now = datetime.datetime.now()
-#timezone = pytz.timezone("Europe/Moscow")
-#print(f'now: {now}')
-#print(f'type of now: {type(now)}')
-#print(f'timezone: {timezone}')
-#print(f'type of timezone: {type(timezone)}')
+another_now = datetime.timedelta()
+now = datetime.datetime.now()
+timezone = pytz.timezone("Europe/Moscow")
+print(f'now: {now}')
+print(f'type of now: {type(now)}')
+print(f'timezone: {timezone}')
+print(f'type of timezone: {type(timezone)}')
 
-#now_with_tz = timezone.localize(now)
-#print(f'now_with_tz: {now_with_tz}')
-#print(f'type of now_with_tz: {type(now_with_tz)}')
+now_with_tz = timezone.localize(now)
+print(f'now_with_tz: {now_with_tz}')
+print(f'type of now_with_tz: {type(now_with_tz)}')
 
-#now_formatted = now_with_tz.strftime("%d.%m.%Y")
-#print(f'now_formatted: {now_formatted}')
-#print(f'type of now_formatted: {type(now_formatted)}')
+now_formatted = now_with_tz.strftime("%d.%m.%Y")
+print(f'now_formatted: {now_formatted}')
+print(f'type of now_formatted: {type(now_formatted)}')
 
 now_parsed = datetime.datetime.strptime("29.01.2022", "%d.%m.%Y")
 print(f'now_parsed: {now_parsed}')
-#print(f'type of now_parsed: {type(now_parsed)}')
+print(f'type of now_parsed: {type(now_parsed)}')
 
 
-now_plus_week = now_parsed + relativedelta(weeks=+5)
+now_plus_week = now_parsed + relativedelta(months=+2)
 print('now + week', now_plus_week)
-#print('type of now_plus_week', type(now_plus_week))
+
+while True:
+    command = input('Введите команду, q для выхода\n')
+    if command == 'q':
+        break
+    eval(command)
